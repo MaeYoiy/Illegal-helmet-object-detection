104c3cab05f840ed1bd1c6d1d607e0d563726932
MaeYoiy <b6333034@gmail.com>
2023-09-05 21:30:58 +0700

    edit predict.py and predict test_vid

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ import cv2
 
 VIDEOS_DIR = os.path.join('.', 'videos')
 
-video_path = os.path.join(VIDEOS_DIR, 'D:/Graduation_Project/Illegal-helmet-object-detection/')
+video_path = os.path.join(VIDEOS_DIR, 'D:/Graduation_Project/Illegal-helmet-object-detection/test_vid.mp4')
 video_path_out = '{}_out.mp4'.format(video_path)
 
 cap = cv2.VideoCapture(video_path)
@@ -22,6 +22,30 @@ if not ret:
     exit()
 
 H, W, _ = frame.shape
-out = cv2.VideoWriter(video_path_out, cv2.videoWrier_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
+out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
 
 model_path = os.path.join('.', 'runs', 'detect', 'train10', 'weights', 'best.pt')
+
+# Load a model
+model = YOLO(model_path) # Load a custom model
+
+threshold = 0.5
+
+while ret:
+
+    results = model(frame)[0]
+
+    for result in results.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = result
+
+        if score > threshold:
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+            cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+            
+    out.write(frame)
+    ret, frame = cap.read()
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
